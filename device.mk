a2bdc18c7071fc1349db61426dd23d426531b697
LuK1337 <priv.luk@gmail.com>
2020-09-18 14:36:05 +0200

    op3: Build libhidltransport/libhwbinder
    * Needed for old blobs
    
    Change-Id: I43fc0e46547dfc126a4b2ac9dcc82c7c4f2ee172

--- device.mk
+++ device.mk
@@ -255,7 +255,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
 
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0
+    android.hidl.base@1.0 \
+    libhidltransport \
+    libhwbinder \
 
 # IPv6
 PRODUCT_PACKAGES += \
